def905fbc82c4e718cf25df70de8bce00306dcd7
Gabriel Castro <ghscastro@hotmail.com>
2023-03-22 16:11:39 -0300

    Busca de dados em uma lista.

---
+++ busca_dados.py
@@ -0,0 +1,20 @@
+eqptos = []
+valores = []
+seriais = []
+deptos = []
+resp = "S"
+while resp == "S":
+ eqptos.append(input("Equipamento: "))
+ valores.append(float(input("Valor: ")))
+ seriais.append(int(input("Número Serial: ")))
+ deptos.append(input("Departamento: "))
+ resp = input("Digite \"S\" para continuar: ").upper()
+
+busca = input("\nDigite o nome do equipamento que deseja visualizar: ")
+for indice in range (0, len(eqptos)):
+    if busca == eqptos[indice]:
+        print("\nEquipamento: ", (indice+1))
+        print("Nome: ", eqptos[indice])
+        print("Valor: ", valores[indice])
+        print("Serial: ", seriais[indice])
+        print("Departamento: ", deptos[indice])
